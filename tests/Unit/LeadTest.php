8c23062fe5c11223ad5c5a57b0a303b829f1930a
Eduar Bastidas <eduarbastidas10@gmail.com>
2022-10-01 12:56:13 -0400

    Resolved existing test conflicts

--- tests/Unit/LeadTest.php
+++ tests/Unit/LeadTest.php
@@ -4,7 +4,6 @@ use Tepuilabs\SimpleCrm\Models\Enums\Lead\LeadStatus;
 use Tepuilabs\SimpleCrm\Models\Enums\Lead\LeadType;
 use Tepuilabs\SimpleCrm\Models\Lead;
 
-<<<<<<< HEAD
 beforeEach(function () {
     $this->lead = Lead::factory()
         ->organicType()
@@ -22,24 +21,4 @@ test('it can create a new lead', function () {
 
 test('it can create a new lead as organic', function () {
     $this->assertEquals(LeadType::ORGANIC_TYPE(), $this->lead->type);
-=======
-test('it can create lead', function () {
-    $lead = Lead::factory()->create();
-
-    $this->assertInstanceOf(Lead::class, $lead);
-});
-
-test('it can create a new lead as statusProspect typeOrganic', function () {
-    $lead = Lead::factory()->statusProspect()->typeOrganic()->create();
-
-    expect($lead->type->types())->toBe('organic');
-    expect($lead->status->status())->toBe('prospect');
-});
-
-test('it can create a new lead as typeUserSubmitted statusLead', function () {
-    $lead = Lead::factory()->typeUserSubmitted()->statusLead()->create();
-
-    expect($lead->type->types())->toBe('user submitted');
-    expect($lead->status->status())->toBe('lead');
->>>>>>> master
 });
--- tests/Unit/NoteTest.php
+++ tests/Unit/NoteTest.php
@@ -1,6 +1,5 @@
 <?php
 
-<<<<<<< HEAD
 use Tepuilabs\SimpleCrm\Models\Enums\Note\NotePriority;
 use Tepuilabs\SimpleCrm\Models\Lead;
 use Tepuilabs\SimpleCrm\Models\Note;
@@ -17,22 +16,6 @@ beforeEach(function () {
         'title' => 'Some title',
         'body' => 'Some body',
         'lead_id' => $this->lead->id,
-=======
-use Tepuilabs\SimpleCrm\Models\Enums\NoteStatus;
-use Tepuilabs\SimpleCrm\Models\Lead;
-use Tepuilabs\SimpleCrm\Models\Note;
-use Tepuilabs\SimpleCrm\Tests\Models\User;
-
-test('if a user can create a note', function () {
-    $user = User::factory()->create();
-    $lead = Lead::factory()->create();
-
-    $note = $user->notes()->create([
-        'title' => 'Some title',
-        'body' => 'Some body',
-        'lead_id' => $lead->id,
-        'priority' => NoteStatus::LOW_PRIORITY(),
->>>>>>> master
     ]);
 });
 
